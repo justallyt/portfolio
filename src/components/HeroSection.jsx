edd4204f93b6d6b61254fb8872eb5f8833a2a7bd
justallyt <albertjunk2030@gmail.com>
2023-06-11 23:52:20 +0300

    Initial Commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,17 +1,25 @@
 import { IoNewspaperOutline } from 'react-icons/io5'
-
-
+import heroImg from "../assets/heroImg.jpg"
 const HeroSection = () => {
   return (
     <div className="hero-section">
                  <div className="inner-row">
                               <div className="hero-section-content">
                                            <div className="hero-texts">
-                                                      <h3>Hi, my name is</h3>
-                                                      <h1>Albert Okundi.</h1>
-                                                      <p>I  am a Frontend Engineer who builds amazing user interfaces that provide the best possible user experience for different brands.</p>
+                                                   <div className="hero-texts-inner">
+                                                             <h3>Hi, my name is</h3>
+                                                             <h1>Albert Okundi.</h1>
+                                                             <p>I  am a Frontend Engineer who builds amazing user interfaces that provide the best possible user experience for different brands.</p>
 
-                                                      <a href="s"><span><IoNewspaperOutline /></span> Resume</a>
+                                                             <a href="s"><span><IoNewspaperOutline /></span> Resume</a>
+                                                   </div>
+                                           </div>
+                                           <div className="hero-image">
+                                                       <div className="hero-image-inner">
+                                                                  <div className="image-self">
+                                                                                <img src={heroImg} alt="" />
+                                                                  </div>
+                                                       </div>
                                            </div>
                               </div>
                  </div>
