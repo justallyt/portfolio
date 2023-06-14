6aad5cf4436a6b31b54d844598e9400e79b37222
justallyt <albertjunk2030@gmail.com>
2023-06-15 00:52:30 +0300

    Updates

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,22 +1,45 @@
-import { IoNewspaperOutline } from 'react-icons/io5'
+import { ImGithub } from "react-icons/im"
+import { BsLinkedin, BsTwitter } from "react-icons/bs"
+import  { AiFillInstagram } from "react-icons/ai"
+import { GiCloudDownload } from 'react-icons/gi'
+import { NavLink } from "react-router-dom"
 import heroImg from "../assets/heroImg.png"
 const HeroSection = () => {
   return (
     <div className="hero-section">
                  <div className="inner-row">
                               <div className="hero-section-content">
+                                         
                                           <div className="circular-lines">
                                                     <div className="circular-line-one"></div>
                                                     <div className="circular-line-two"></div>
                                           </div>
+                                          <div className="small-bubble"></div>
                                         <div className="heading">
                                                   <h1>Hi, <br /> I&apos;m Albert</h1>
                                                   <p> I build amazing user interfaces that create the best possible user experience for various products on the web.</p>
                                         </div>
-                                       <div className="hero-imagery-shadow"> 
-                                         
+                                        <div className="bubble">
+                                                 <NavLink to={'/'}>Resume <span><GiCloudDownload /></span></NavLink>
+                                        </div>
+                                        <div className="links-muhimu">
+                                                      <ul>
+                                                             <li className="active"><NavLink to={'/'}><ImGithub /></NavLink></li>
+                                                             <li className="active"><NavLink to={'/'}><BsLinkedin /></NavLink></li>
+                                                             <li className="active"><NavLink to={'/'}><BsTwitter /></NavLink></li>
+                                                             <li className="active"><NavLink to={'/'}><AiFillInstagram /></NavLink></li>
+                                                      </ul>
+                                        </div>
+                                       <div className="hero-imagery-shadow">   </div>
+                                       <div className="notable-texts">
+                                                     
+                                                     <div className="small-box special">
+                                                              Frontend Engineer
+                                                     </div>
+                                                     <div className="small-box">
+                                                                5+ Years of Experience
+                                                     </div>
                                        </div>
-                                     
                                        <div className="hero-image">
                                                   <img src={heroImg} alt="" />
                                         </div>
