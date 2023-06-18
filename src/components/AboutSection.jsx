d490cef783fc95f6ccf27cc07ca95507d3574269
justallyt <albertjunk2030@gmail.com>
2023-06-18 23:16:56 +0300

    Updates

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -8,6 +8,10 @@ const AboutSection = () => {
                                                      <h3>About Me</h3>
                                            </div>
                                            <h1>On a mission to paint the web into a beautiful canvas one web project at a time.</h1>
+                                           <div className="explanation-texts">
+                                                      <p>Hello, My name is <span className="underlined">Albert Okundi</span>, I build things for the web. Over the years, I&apos;ve built products for companies and businesses around the globe ranging from marketing websites to complex solutions and enterprise apps with focus on fast, elegant and accessible user experiences.</p>
+                                                      <p>My technical background allows me to understand the complexities of web development, and I&apos;m always on the lookout for the latest trends and technologies in my field, to consistently learn and seek out ways to improve my skills and improve the projects I&apos;m engaged in.</p>
+                                           </div>
                             </div>
                </div>
     </div>
