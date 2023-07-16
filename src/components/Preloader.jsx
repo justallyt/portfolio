37d8b93a58087d70f2943f6c3023d088163f8bfd
justallyt <albertjunk2030@gmail.com>
2023-07-16 18:36:33 +0300

    Updates

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useEffect } from "react"
 import { useRef } from "react"
-import { gsap } from "gsap"
+import { gsap, Power4 } from "gsap"
 const Preloader = () => {
     const [preloaderCols, setPreloaderCols] = useState()
     const [preloaderLine, setPreloaderLine] = useState()
@@ -16,8 +16,12 @@ const Preloader = () => {
            setPreloaderLine(LineRef.current);
     }, [])
 
-   let initial, done = false, prevTimeStamp, initialLoadT = new Date().getTime()
-   
+   let initial, 
+   done = false,
+    prevTimeStamp, 
+    initialLoadT = new Date().getTime(),
+    tl = gsap.timeline()
+  
     const playLoader = (timeStamp) => {
             const loadAfterT = new Date().getTime();
             const pageload = initialLoadT - loadAfterT
@@ -34,7 +38,39 @@ const Preloader = () => {
                   if(preloaderBar !== undefined){
                            preloaderBar.style.width = `${count}%`
                   }
-                  if (count === 100) done = true
+                  if (count === 100){
+                         done = true
+                        
+                         if(preloaderBar && preloaderLine && preloaderCols){
+                                  tl.to(preloaderBar, {
+                                          opacity: 0,
+                                          duration: 0.5
+                                  })
+                                  tl.to(preloaderCols.querySelector(".name-box h2"), {
+                                         opacity: 0,
+                                         duration: 0.3
+                                  })
+                                  tl.to(preloaderLine, {
+                                         width: '100%',
+                                         duration: 0.8,
+                                         ease: Power4.easeOut,
+                                  })
+                          
+                                  if(preloaderCols){
+                                        const cols = preloaderCols.querySelectorAll(".preloader-col")
+                                        for(let i = 0 ; i < cols.length; i++){
+                                               tl.to(cols[0], {
+                                                      y: '-100%',
+                                                     duration: 0.5
+                                                })
+                                              tl.to(cols[1], {
+                                                      y: '100%',
+                                                      duration: 0.5
+                                              })
+                                         }
+                                 }
+                         }
+                  }
             }
 
             if(elapsed < Math.abs(pageload)){
